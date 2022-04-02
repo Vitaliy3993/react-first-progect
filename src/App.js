4855c4ba3147656a349669bf3ce9cfb0cf9e17d4
vitaliy3993 <kadelchukkp@gmail.com>
2022-04-02 17:03:27 +0300

    test1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import logo from './logo.svg';
+import {Component, StrictMode} from 'react';
 import './App.css';
+
+const Header = () => {
+    return <h2>Hello world!</h2>
+}
+
+/* const Field = () => {
+  const holder = 'Enter here';
+  const styledField = {
+    width: '300px'
+  };
+  return <input 
+    placeholder={holder} 
+    type='text' 
+    style={styledField}/>
+} */
+
+class Field extends Component {
+  render() {
+    const holder = 'Enter here';
+    const styledField = {
+      width: '300px'
+    };
+
+    return <input 
+      placeholder={holder} 
+      type='text' 
+      style={styledField}/>
+  }
+}
+
+function Btn() {
+  const text = 'Log in';
+  const logged = false;
+  // const res = () => {
+  //   return 'Log in';
+  // }
+  // const p = <p>Log in</p>
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <StrictMode>
+        <Header/>
+      </StrictMode>
+      <Field/>
+      <Btn/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +62,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
 
+export {Header};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Header} from './App';
 
-const elem = <h2>Hello world!</h2>;
+/* const text = 'Hello world!';
+
+const elem = (
+  <div>
+    <h2>Text: {text}</h2>
+    <input type="text" />
+    <button>Click</button>
+  </div>
+); */
 
 ReactDOM.render(
-  elem,
+  <StrictMode>
+    <App/>
+  </StrictMode>,
+  
   document.getElementById('root')
 );
 
